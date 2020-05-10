2e7e843d5ff842dc1b783bfc48da1541c37a5465
Anusha Neupane <neupaneanushaa@gmail.com>
2020-05-10 13:51:46 -0400

    Adding headline

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("webpage.html")
+	render_this = "Hi whoever is this!!"
+	return render_template("webpage.html", headline=render_this)
 
 if __name__=="__main__":
     app.run(debug=True)
